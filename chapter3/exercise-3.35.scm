19efd4cb6ee955f497d1405c2fd5b261270ee8bd
John Donnellan <codybartfast@gmail.com>
2018-04-14 12:13:54 +0100

    Exercise 3.35

--- chapter3/exercise-3.35.scm
+++ chapter3/exercise-3.35.scm
@@ -29,8 +29,32 @@
 ;   ------------------------------------------------------------------------
 
 (-start- "3.35")
-
-
-
+(prn "
+   (define (squarer a b)
+     (define (process-new-value)
+       (if (has-value? b)
+           (if (< (get-value b) 0)
+               (error \"square less than 0 -- SQUARER\" (get-value b))
+               (set-value! a
+                           (square-root (get-value b))
+                           me))  
+            (if (has-value? a)
+                (set-value! b
+                            (* (get-value a) (get-value a))
+                            me))))
+     (define (process-forget-value)
+       (forget-value! a me)
+       (forget-value! b me)
+       (process-new-value))
+       (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error \"Unknown request -- SQUARER\" request))))
+     (connect a me)
+     (connect b me)
+     me)")
 (--end-- "3.35")
 
