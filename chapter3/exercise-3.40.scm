96845f1c82f057df1a39c895e66163c39e1f505b
John Donnellan <codybartfast@gmail.com>
2018-06-16 10:34:38 +0100

    Exercise 3.40

--- chapter3/exercise-3.40.scm
+++ chapter3/exercise-3.40.scm
@@ -29,7 +29,50 @@
 
 (-start- "3.40")
 
+(prn "Going to work on the assumption that each arguement to a function is
+evaluated independently, even if it's the same expression, i.e. each 'x' is
+evaluated seprately.
 
+So I think the final value for x are:
+100; 1,000; 10,000; 100,000; 1,000,000
+
+With both 'sets' after all evaluations of x.
+(* 10 10)
+(* 10 10 10)
+-> 100, 1,000
+
+With first 'set' befor full evaulation of (* x x x)
+(* 10 10)
+(* 10 10 100)
+-> 10,000
+
+(* 10 10)
+(* 10 100 100)
+-> 100,000
+
+(* 10 10)
+(* 100 100 100)
+-> 1,000,000
+
+With secon 'set' before full evaluation of (* x x)
+(* 10 1,000)
+(* 10 10 10)
+-> 10,000
+
+(* 1,000 1,000)
+(* 10 10 10)
+-> 1,000,000
+
+With the use of serialize only possible final value is 1,000,000 
+
+(* 10 10)
+(* 100 100 100)
+-> 1,000,000
+
+(* 1,000 1,000)
+(* 10 10 10)
+-> 1,000,000
+")
 
 (--end-- "3.40")
 
