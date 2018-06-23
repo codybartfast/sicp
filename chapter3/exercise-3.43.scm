83493e645f458f942985b63c9f32def10ab8ed5a
John Donnellan <codybartfast@gmail.com>
2018-06-23 12:09:57 +0100

    Exercise 3.43

--- chapter3/exercise-3.43.scm
+++ chapter3/exercise-3.43.scm
@@ -25,6 +25,61 @@
 
 (-start- "3.43")
 
+(prn "
+Argue should be $10, $20, and $30 in some order
+===============================================
+No two exchange processes can overlap so each can be reasoned about
+independently.  Each process (when run independently) changes the order, but
+not the values.  Since each individual process leaves the
+combination unchanged any series of processes will also leave the
+combination unchanged.
+
+
+This condition can be violated with first version of code
+=========================================================
+This is the usual story:
+
+(a, b, c)
+
+Exchange a b     Exchange b c
+------------     ------------
+         (10, 20, 30)
+Read 10, 20
+                 Read 20, 30
+<- Trnsfr 10
+         (20, 10, 30)
+                 <- Trnsfr 10
+         (20, 20, 20)
+
+Total value is unchanged because because although the value of 'difference'
+could be wrong (e.g. changes made to accoun2 between the reading of account1
+and account2)  the same vauue ('difference') is passed to both the the
+withdraw and deposit accounts.
+
+
+This condition is violated if we do not serialize the transaction
+=================================================================
+
+In the above example we the to transfers can happen at the same time.  This
+means we could have a withdrawl and a depost to account2 at the same time.
+
+Withdraw 10           Deposit 10
+===========           ==========
+                 20
+Read balance 20
+                      Read blance 20
+                      New balance 30
+                      set! 30
+                 30
+New balance 10
+set! 10
+                 10
+
+As the other two accounts are only touched by one process their values will
+the same as the above example so the final balances will be:
+         (20, 10, 20)
+I.e., $10 has been destroyed.
+")
 
 
 (--end-- "3.43")
