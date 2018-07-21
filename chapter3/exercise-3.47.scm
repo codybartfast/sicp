d4fc6f097ca6ca91391a5502f6fe0de65cfb95f6
John Donnellan <codybartfast@gmail.com>
2018-07-21 11:12:46 +0100

    Exercise 3.47

--- chapter3/exercise-3.47.scm
+++ chapter3/exercise-3.47.scm
@@ -22,7 +22,76 @@
 
 (-start- "3.47")
 
+;; HELPERS
+;; =======
 
+(define (make-mutex)
+  (let ((cell (list false)))            
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      true
+      (begin (set-car! cell true)
+             false)))
+
+
+(prn "
+Using mutex:
+============
+
+(define (make-semaphore max)
+  (let ((mutex (make-mutex))
+        (count 0))
+    (define (dispatch m)
+      (cond ((eq? m 'acquire)
+             (mutex 'acquire)
+             (cond ((< count max)
+                    (set! count (+ count 1))
+                    (mutex 'release))
+                   (else
+                    (mutex 'release)
+                    (dispatch 'acquire))))        
+            ((eq? m 'release)
+             (mutex 'acquire)
+             (set! count (- count 1))
+             (mutex 'release))
+            (else (error \"Unknown request -- SAMAPHORE\" m))))
+    dispatch)))
+
+
+Using test-and-set!
+===================
+
+(define (make-semaphore max)
+  (let ((cell (list false))
+        (count 0))
+    (define (dispatch m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (dispatch'acquire))
+             (cond ((< count max)
+                    (set! count (+ count 1))
+                    (clear! cell))
+                   (else
+                    (clear! cell)
+                    (dispatch 'acquire))))        
+            ((eq? m 'release)
+             (if (test-and-set! cell)
+                 (dispatch'release))
+             (set! count (- count 1))
+             (clear! cell))
+            (else (error \"Unknown request -- SAMAPHORE\" m))))
+    dispatch))
+")
 
 (--end-- "3.47")
 
