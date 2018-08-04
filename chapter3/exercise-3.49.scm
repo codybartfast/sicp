ae92c349e1fb9d17b22d48c757603aab3c6ccfdf
John Donnellan <codybartfast@gmail.com>
2018-08-04 11:19:00 +0100

    Exercise 3.49

--- chapter3/exercise-3.49.scm
+++ chapter3/exercise-3.49.scm
@@ -18,7 +18,17 @@
 
 (-start- "3.49")
 
+(prn "
+Consider a program that attempts to find a chain of home owners who wish to
+exchange their houses e.g. A's house -> B, B -> C, ...., Y -> Z, Z -> A.
 
+Also consider that you each house owner changes their mind about which house
+they want each time they are asked.  So 'A' has to be locked between finding
+out what they want to get and fullfilling that request.
+
+If I lock 'N' and find that it wants to buy 'M', then to I need to releae N
+before I lock M, but when I relock N it no longer wants to mbuy M.
+")
 
 (--end-- "3.49")
 
