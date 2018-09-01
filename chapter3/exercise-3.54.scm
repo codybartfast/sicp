35f18183cc064ca5dce92546333400bdcf7fa390
John Donnellan <codybartfast@gmail.com>
2018-09-01 12:10:32 +0100

    Exercise 3.54

--- chapter3/exercise-3.54.scm
+++ chapter3/exercise-3.54.scm
@@ -19,7 +19,15 @@
 
 (-start- "3.54")
 
+(prn "
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2)) 
 
+(define factorials
+  (cons-stream 1 (mul-streams
+                  (integers-starting-from 2)
+                  factorials)))
+")
 
 (--end-- "3.54")
 
