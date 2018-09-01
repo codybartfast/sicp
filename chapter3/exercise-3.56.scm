bf6689a391ecdd82d5bee4841c511a6acd7b6188
John Donnellan <codybartfast@gmail.com>
2018-09-01 12:45:12 +0100

    Exercise 3.56

--- chapter3/exercise-3.56.scm
+++ chapter3/exercise-3.56.scm
@@ -54,6 +54,14 @@
 
 (-start- "3.56")
 
+(prn "
+
+(define S (cons-stream 1
+                       (merge
+                        (scale-stream S 2)
+                        (merge (scale-stream S 3)
+                               (scale-stream S 5)))))
+")
 
 
 (--end-- "3.56")
