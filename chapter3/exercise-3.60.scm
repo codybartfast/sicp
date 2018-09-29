c2cc37ed08bf5717937d3183a0f823734cc873b3
John Donnellan <codybartfast@gmail.com>
2018-09-29 12:43:53 +0100

    3.60 add mul-series

--- chapter3/exercise-3.60.scm
+++ chapter3/exercise-3.60.scm
@@ -23,7 +23,11 @@
 
 (-start- "3.60")
 
-
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+               (add-stream
+                (scale-stream (stream-cdr s2) (stream-car s1))
+                (mul-series (stream-cdr s1) s2))))
 
 (--end-- "3.60")
 
