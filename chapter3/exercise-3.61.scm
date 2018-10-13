08b30b257c82a63167473bb5b166b78e8424e509
John Donnellan <codybartfast@gmail.com>
2018-10-13 11:30:31 +0100

    Exercise 3.61

--- chapter3/exercise-3.61.scm
+++ chapter3/exercise-3.61.scm
@@ -30,7 +30,15 @@
 
 (-start- "3.61")
 
-
+(prn "
+(define (invert-unit-series S)
+  (if (= 1 (stream-car S))
+         (cons-stream 1
+                      (scale-stream
+                       (mul-series (stream-cdr S) (invert-unit-series S))
+                       -1))
+         (error \"Constant term of series must be 1\")))
+ ")    
 
 (--end-- "3.61")
 
