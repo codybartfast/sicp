9a84381b012ff23e0fb59fc9784bf9f4f51fd659
John Donnellan <codybartfast@gmail.com>
2019-01-15 17:53:27 +0000

    Exercise 3.77

--- chapter3/exercise-3.77.scm
+++ chapter3/exercise-3.77.scm
@@ -32,7 +32,47 @@
 
 (-start- "3.77")
 
+(prn "Despite the (to me) complex context we have, e.g., mutually defined
+streams, calculus, RC circuits etc... all we need to do here is avoid a
+direct reference to 'integrand' because 1) we have applicative order
+evaluation of arguments and 2) when using 'solve' the integrand is not yet
+defined.  So we just need to make sure integrand isn't referenced
+until after the first item is returned from the stream.
 
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (stream-cdr integrand)
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt)))))
 
+But integral is now expecting a deley-ed function (rather than a cons
+pair). The correct way to do this porbably to use delay: 
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (delay (stream-cdr integrand))
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt)))))
+
+However, if '(cons-stream <a> <b>)' is precisely equivalent to
+'(cons <a> (delay <b>))' then perhaps the following might also work:
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (cdr integrand)
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt)))))")
 (--end-- "3.77")
 
