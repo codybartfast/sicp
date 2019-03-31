747a85acc8a304a168e3d7cd0837b5c30c7c6852
John Donnellan <codybartfast@gmail.com>
2019-03-31 12:46:54 +0100

    4.03-ext move ea-eval expr check to expr file
    Check eval test expression with text's eval in the expression module to
    avoid 'eval' name clashes when checking it in exercise file.

--- chapter4/ea-pick-fruit-expression.scm
+++ chapter4/ea-pick-fruit-expression.scm
@@ -1,27 +1,23 @@
 #lang sicp
 
+(#%require "ea-eval-apply.scm")
 
 (define pick-fruit
   '(lambda ()
-    (define trace '())
-
-    (define (get-apple)
-      (set! trace (cons "'getting apple'" trace))
-      "apple")
-
-    (define (get-cherry)
-      (set! trace (cons "'getting cherry'" trace))
-      "cherry")
-
-    (define (first-or-second first second which)
-      (cond ((equal? which 'first) (first))
-            (else (second))))
-
-    ;(define (result)
-      (list
-       (first-or-second get-apple get-cherry 'first)
-       (first-or-second get-apple get-cherry 'not-first)
-       trace)))
+     (define trace '())
+     (define (get-apple)
+       (set! trace (cons "'getting apple'" trace))
+       "apple")
+     (define (get-cherry)
+       (set! trace (cons "'getting cherry'" trace))
+       "cherry")
+     (define (first-or-second first second which)
+       (cond ((equal? which 'first) (first))
+             (else (second))))
+     (list
+      (first-or-second get-apple get-cherry 'first)
+      (first-or-second get-apple get-cherry 'not-first)
+      trace)))
    
 
 (define (check-fruit result)
@@ -35,6 +31,14 @@
             '("'getting cherry'" "'getting apple'")
             (caddr result)))
   (newline))
+
+(display
+ "Checking expression with eval from text:")(newline)
+(check-fruit
+ (apply (eval
+         pick-fruit
+         the-global-environment)
+        '()))
 
 (#%provide
  pick-fruit
