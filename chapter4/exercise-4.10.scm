d5f70ecbf4a8a7483d925e65efa708b5464c4772
John Donnellan <codybartfast@gmail.com>
2019-04-19 12:54:26 +0100

    Exercise 4.10

--- chapter4/exercise-4.10.scm
+++ chapter4/exercise-4.10.scm
@@ -18,7 +18,64 @@
 
 (-start- "4.10")
 
+(println "
+Below is an example of changing the name of a proc (let -> with) and it's
+straight forward.  One could quite easily reorder paramaters e.g., change
+named let from (let name pairs body) to (let pairs name body).  But apart
+from renaming and reodering stuff, other changes my limited imagination can
+summon just seem to make the language less usable and harder to implement.")
 
+(#%require "ea-data-directed.scm")
+(put-evaluators)
+
+;; Rename let to 'with' ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define make-call cons)
+(define with-pairs cadr)
+(define with-pair-id car)
+(define with-pair-value cadr)
+
+(define (with-params exp)
+  (map with-pair-id (with-pairs exp)))
+(define (with-values exp)
+  (map with-pair-value (with-pairs exp)))
+(define with-body cddr)
+
+(define (with->combination exp)
+  (make-call
+   (make-lambda (with-params exp)
+                (with-body exp))
+   (with-values exp)))
+
+(define (eval-with exp env)
+  (eval (with->combination exp) env))
+
+(put 'eval 'with eval-with)
+
+;;  ;;;;;;;
+
+(define (last list)
+  (if (equal? '() (cdr list))
+      (car list)
+      (last (cdr list))))
+
+;; Use it ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define with-expr '(with ((operand1 3)
+                          (operator +)
+                          (operand2 2))
+                      'first-line
+                      (+ 1 1)
+                      (operator operand1 operand2)))
+
+(println "
+Evaluating expression:
+    " with-expr "
+Expect: 5
+Got: "
+      (eval
+       with-expr
+       the-global-environment))
 
 (--end-- "4.10")
 
