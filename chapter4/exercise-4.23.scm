3f284e5861ae7c6235fb338afe5d510ebc76aa12
John Donnellan <codybartfast@gmail.com>
2019-05-01 19:28:38 +0100

    Exercise 4.23

--- chapter4/exercise-4.23.scm
+++ chapter4/exercise-4.23.scm
@@ -43,7 +43,25 @@
 
 (-start- "4.23")
 
+(println "
+Analysis of the individual expressions still only happens once as procs is
+only created once.
 
+But Alyssa's always does more work at execution time but it doesn't seem a
+significant amount.  With one expression Eva's immediately calls the the
+proc for that first expression, but Alyssa's has to first call
+execute-sequence then check whether it is the last proc (cdr and a null
+comparison) before calling car
+
+With two items we have two calls, two null comaparisons, two calls to car
+and two call to cdr.  Conceiveably we could have cond being repeatedly being
+rewritten to if and begin statements but we can probalby assume the
+executing version is also using analysis.  In contrast Eva's just has one
+call to loop in addition to the procs.
+
+This iteration over the list of procs doesn't seem as though it would ever
+be expensive, but if the procedures were themselves short and the sequence
+were heavily recusive then perhaps it could be significant.")
 
 (--end-- "4.23")
 
