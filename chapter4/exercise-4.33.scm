7cdea55b40517f99b3ab335950666efc9649a7b6
John Donnellan <codybartfast@gmail.com>
2019-06-08 13:29:05 +0100

    4.33 - working?

--- chapter4/exercise-4.33.scm
+++ chapter4/exercise-4.33.scm
@@ -29,18 +29,24 @@
 (define (quoted-exp exp) (cadr exp))
 (define (quoted-pair? exp) (pair? (quoted-exp exp)))
 
+(define (make-quote exp)
+  (list 'quote exp))
 
 (define (eval-quotation exp env)
-  (println "------------------------------------")
-  (println "Expression: " exp)
-  (println "Q-expression: " (quoted-exp exp) ", Pair?: " (quoted-pair? exp))
-  (println "====================================")
+;  (println "------------------------------------")
+;  (println "Expression: " exp)
+;  (println "Q-expression: " (quoted-exp exp) ", Pair?: " (quoted-pair? exp))
+;  (println "====================================")
 
   (let ((q-exp (quoted-exp exp))
         (q-pair? (quoted-pair? exp)))
     (if (not q-pair?)
         q-exp
-        q-exp)))
+        (eval (make-lambda (list 'm)
+                           (list
+                            (list 'm
+                                  (make-quote (car q-exp))
+                                  (make-quote (cdr q-exp))))) env))))
    
 
 (put 'eval 'quote eval-quotation)
@@ -51,22 +57,26 @@
      (define (cons (x lazy-memo) (y lazy-memo))
        (lambda (m) (m x y)))
      (define (car z)
+       ;(println "car!!: " z)
        (z (lambda (p q) p)))
      (define (cdr z)
        (z (lambda (p q) q)))
+     (define (cadr z) (car (cdr z)))
+     (define (caddr z) (car (cdr (cdr z))))
 
      (println 'xyx)
-     (println '(a b c))
+     (println (car '(a b c)))
+     (println (cadr '(a b c)))
+     (println (caddr '(a b c)))
 
      ))
 
 (eval program the-global-environment)
 
-(define (make-quote exp)
-  (list 'quote exp))
 
-(println (eval-quotation (make-quote 'xyz) '()))
-(println (eval-quotation (make-quote '(a b c)) '()))
+
+;(println (eval-quotation (make-quote 'xyz) '()))
+;(println (eval-quotation (make-quote '(a b c)) '()))
 
 (--end-- "4.33")
 
