140dbb946fe44bffbb2cb6884073bba42bbcf5c6
John Donnellan <codybartfast@gmail.com>
2019-06-22 12:20:10 +0100

    4.34 - wip

--- chapter4/exercise-4.34.scm
+++ chapter4/exercise-4.34.scm
@@ -11,13 +11,95 @@
 ;   pairs so that the evaluator can identify them in order to print them.
 ;   
 ;   ------------------------------------------------------------------------
-;   [Exercise 4.34]: http://sicp-book.com/book-Z-H-27.html#%_thm_4.34
+;   [Exercise 4.34]: http:/make/sicp-book.com/book-Z-H-27.html#%_thm_4.34
 ;   4.2.3 Streams as Lazy Lists - p411
 ;   ------------------------------------------------------------------------
 
 (-start- "4.34")
 
+(#%require "ea-data-directed-34.scm")
+(put-evaluators)
 
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+;(define (user-print object)
+;  (if (compound-procedure? object)
+;      (display (list 'compound-procedure
+;                     (procedure-parameter-defs object)
+;                     (procedure-body object)
+;                     '<procedure-env>))
+;      (display object)))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (user-print-list object 4)
+      (display 'other)))
+
+(define (user-print-list object count)
+  (display (apply object '((lambda (p q) p)) the-global-environment))
+  (newline)
+  (display 'done)
+  )
+
+(define (lcar z)
+  (z (lambda (p q) p)))
+(define (lcdr z)
+  (z (lambda (p q) q)))
+
+(define program
+  '(begin
+
+     (define (cons (x lazy-memo) (y lazy-memo))
+       (lambda (m) (m x y)))
+     (define (car z)
+       (z (lambda (p q) p)))
+     (define (cdr z)
+       (z (lambda (p q) q)))
+
+     (define list1
+       (cons 'apple '()))
+     
+     (define list2
+       (cons 'apple
+             (cons 'banana '())))
+
+     (define list4
+       (cons 'apple
+             (cons 'banana
+                   (cons 'cherry
+                         (cons 'durian '())))))
+
+     (define list8
+       (cons 'apple
+             (cons 'banana
+                   (cons 'cherry
+                         (cons 'duck
+                               (cons 'elk
+                                     (cons 'fox
+                                           (cons 'grape
+                                                 (cons 'horse '())))))))))
+
+     (define (add2) (+ 2 0))
+
+     ))
+
+(eval program the-global-environment)
+
+
+(driver-loop)
 
 (--end-- "4.34")
 
