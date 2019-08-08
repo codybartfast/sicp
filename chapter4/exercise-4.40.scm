3f77668aa5255164dc185995981675664a6456fb
John Donnellan <codybartfast@gmail.com>
2019-08-08 16:09:40 +0100

    Exercise 4.40

--- chapter4/exercise-4.40.scm
+++ chapter4/exercise-4.40.scm
@@ -24,7 +24,43 @@
 
 (-start- "4.40")
 
+(println "
+There are 5^5 or 3,125 sets of assignments before the distinct requirement
+and 5! or 120 sets of assignments afterwards.
+")
 
+(define (require p)
+  (if (not p) (amb)))
+
+(define (distinct? items)
+  (cond ((null? items) true)
+        ((null? (cdr items)) true)
+        ((member (car items) (cdr items)) false)
+        (else (distinct? (cdr items)))))
+
+(define (multiple-dwelling)
+  (let ((fletcher (amb 1 2 3 4 5)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (let ((cooper (amb 1 2 3 4 5)))
+      (require (not (= cooper 1)))
+      (require (not (= (abs (- fletcher cooper)) 1)))
+      (let ((miller (amb 1 2 3 4 5)))
+        (require (> miller cooper))
+        (let ((smith (amb 1 2 3 4 5)))
+          (require (not (= (abs (- smith fletcher)) 1)))
+          (let ((baker (amb 1 2 3 4 5)))
+            (require (not (= baker 5)))
+            (require
+              (distinct? (list baker cooper fletcher miller smith)))
+            (list (list 'baker baker)
+                  (list 'cooper cooper)
+                  (list 'fletcher fletcher)
+                  (list 'miller miller)
+                  (list 'smith smith))))))))
+    
+
+(time (multiple-dwelling))
 
 (--end-- "4.40")
 
