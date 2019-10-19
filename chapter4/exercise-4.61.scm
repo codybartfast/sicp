99796a22d2af10dfe54603a4c5fbb5293591fc23
John Donnellan <codybartfast@gmail.com>
2019-10-19 09:39:42 +0100

    Exercise 4.61

--- chapter4/exercise-4.61.scm
+++ chapter4/exercise-4.61.scm
@@ -26,7 +26,15 @@
 
 (-start- "4.61")
 
+Expected response for (?x next-to ?y in (1 (2 3) 4)):
 
+    (1 next-to (2 3) in (1 (2 3) 4))
+    ((2 3) next to 4 in (1 (2 3) 4))
+
+Expected response for (?x next-to ?y in (2 1 3 1)):
+
+    (2 next 1 in (2 1 3 1))
+    (3 next 1 in (2 1 3 1))
 
 (--end-- "4.61")
 
