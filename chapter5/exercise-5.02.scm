1bdfdfcea15420ed0c837f3cc5f24b4ce64d89fe
John Donnellan <codybartfast@gmail.com>
2020-01-14 17:29:35 +0000

    Exercise 5.02

--- chapter5/exercise-5.02.scm
+++ chapter5/exercise-5.02.scm
@@ -4,10 +4,10 @@
 
 ;   Exercise 5.2
 ;   ============
-;   
+;
 ;   Use the register-machine language to describe the iterative factorial
 ;   machine of exercise [5.1].
-;   
+;
 ;   ------------------------------------------------------------------------
 ;   [Exercise 5.2]:  http://sicp-book.com/book-Z-H-31.html#%_thm_5.2
 ;   [Exercise 5.1]:  http://sicp-book.com/book-Z-H-31.html#%_thm_5.1
@@ -16,7 +16,14 @@
 
 (-start- "5.2")
 
-
+(controller
+ test-c
+   (test (op >) (reg c) (reg n))
+   (branch (label factorial-done))
+   (assign p (op mul) (reg p) (reg c))
+   (assign c (op add) (reg c) (const 1))
+   (goto (label test-c))
+ factorial-done)
 
 (--end-- "5.2")
 
