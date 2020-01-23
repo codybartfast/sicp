7844a686b128353e9fd738201f1c541dab1d7e31
John Donnellan <codybartfast@gmail.com>
2020-01-23 10:49:09 +0000

    Exercise 5.11b

--- chapter5/exercise-5.11a.scm
+++ chapter5/exercise-5.11a.scm
@@ -21,16 +21,9 @@
 ;   eliminate one instruction from the Fibonacci machine of section [5.1.4]
 ;   (figure [5.12]).
 ;   
-;   b.  (restore y) puts into y the last value saved on the stack, but only
-;   if that value was saved from y; otherwise, it signals an error.  Modify
-;   the simulator to behave this way.  You will have to change save to put
-;   the register name on the stack along with the value.
-;   
-;   c.  (restore y) puts into y the last value saved from y regardless of
-;   what other registers were saved after y and not restored.  Modify the
-;   simulator to behave this way.  You will have to associate a separate
-;   stack with each register.  You should make the initialize-stack
-;   operation initialize all the register stacks.
+;   b.  <snip>
+;
+;   c.  <snip>
 ;   
 ;   ------------------------------------------------------------------------
 ;   [Exercise 5.11]: http://sicp-book.com/book-Z-H-32.html#%_thm_5.11
--- chapter5/exercise-5.11b.scm
+++ chapter5/exercise-5.11b.scm
@@ -15,22 +15,14 @@
 ;   
 ;   There are several reasonable possibilities for the meaning of restore:
 ;   
-;   a.  (restore y) puts into y the last value saved on the stack,
-;   regardless of what register that value came from.  This is the way our
-;   simulator behaves.  Show how to take advantage of this behavior to
-;   eliminate one instruction from the Fibonacci machine of section [5.1.4]
-;   (figure [5.12]).
+;   a.  <snip>
 ;   
 ;   b.  (restore y) puts into y the last value saved on the stack, but only
 ;   if that value was saved from y; otherwise, it signals an error.  Modify
 ;   the simulator to behave this way.  You will have to change save to put
 ;   the register name on the stack along with the value.
 ;   
-;   c.  (restore y) puts into y the last value saved from y regardless of
-;   what other registers were saved after y and not restored.  Modify the
-;   simulator to behave this way.  You will have to associate a separate
-;   stack with each register.  You should make the initialize-stack
-;   operation initialize all the register stacks.
+;   c.  <snip>
 ;   
 ;   ------------------------------------------------------------------------
 ;   [Exercise 5.11]: http://sicp-book.com/book-Z-H-32.html#%_thm_5.11
@@ -41,7 +33,128 @@
 
 (-start- "5.11")
 
+(#%require "machine-11b.scm")
 
+(println
+ "
+Part B
+======
 
+As implied in the answer we put the register name and its value on the stack
+and then verify the register name at restore time:
+
+  (define (make-save inst machine stack pc)
+    (let* ((reg-name (stack-inst-reg-name inst))
+           (reg (get-register machine reg-name)))
+      (lambda ()
+        (push stack (make-stack-entry reg-name (get-contents reg)))
+        (advance-pc pc))))
+  (define (make-restore inst machine stack pc)
+    (let* ((reg-name (stack-inst-reg-name inst))
+           (reg (get-register machine reg-name)))
+      (lambda ()
+        (let ((stack-entry (pop stack)))
+          (cond
+            ((not
+              (eq? reg-name (stack-entry-reg-name stack-entry)))
+             (error
+              \"cannot restore stack to different register -- ASSEMBLE\"
+              stack-entry))
+            (else
+             (set-contents! reg (stack-entry-value stack-entry))
+             (advance-pc pc)))))))
+  (define (stack-inst-reg-name stack-instruction)
+    (cadr stack-instruction))
+
+  (define (make-stack-entry reg-name value)
+    (cons reg-name value))
+  (define (stack-entry-reg-name stack-entry)
+    (car stack-entry))
+  (define (stack-entry-value stack-entry)
+    (cdr stack-entry))
+
+Original Fibonacci still works (fib 10): "
+
+         (let ((n 10)
+               (machine (make-machine
+                         '(n val continue)
+                         (list
+                          (list '+ +)
+                          (list '- -)
+                          (list '< <))
+                         '((assign continue (label fib-done))
+                           fib-loop
+                           (test (op <) (reg n) (const 2))
+                           (branch (label immediate-answer))
+                           (save continue)
+                           (assign continue (label afterfib-n-1))
+                           (save n)
+                           (assign n (op -) (reg n) (const 1))
+                           (goto (label fib-loop))
+                           afterfib-n-1
+                           (restore n)
+                           (restore continue)
+                           (assign n (op -) (reg n) (const 2))
+                           (save continue)
+                           (assign continue (label afterfib-n-2))
+                           (save val)
+                           (goto (label fib-loop))
+                           afterfib-n-2
+                           ;(restore n)
+                           (assign n (reg val))
+                           (restore val)
+                           (restore continue)
+                           (assign val (op +) (reg val) (reg n)) 
+                           (goto (reg continue))
+                           immediate-answer
+                           (assign val (reg n))
+                           (goto (reg continue))
+                           fib-done))))
+           (set-register-contents! machine 'n n)
+           (start machine)
+           (get-register-contents machine 'val))
+"
+
+But with the optimization form Part A we now EXPECT an ERROR, (fib 10):
+                                             ===============")
+(let ((n 10)
+      (machine (make-machine
+                '(n val continue)
+                (list
+                 (list '+ +)
+                 (list '- -)
+                 (list '< <))
+                '((assign continue (label fib-done))
+                  fib-loop
+                  (test (op <) (reg n) (const 2))
+                  (branch (label immediate-answer))
+                  (save continue)
+                  (assign continue (label afterfib-n-1))
+                  (save n)
+                  (assign n (op -) (reg n) (const 1))
+                  (goto (label fib-loop))
+                  afterfib-n-1
+                  (restore n)
+                  (restore continue)
+                  (assign n (op -) (reg n) (const 2))
+                  (save continue)
+                  (assign continue (label afterfib-n-2))
+                  (save val)
+                  (goto (label fib-loop))
+                  afterfib-n-2
+                  (restore n)
+                  ;(assign n (reg val))
+                  ;(restore val)
+                  (restore continue)
+                  (assign val (op +) (reg val) (reg n)) 
+                  (goto (reg continue))
+                  immediate-answer
+                  (assign val (reg n))
+                  (goto (reg continue))
+                  fib-done))))
+  (set-register-contents! machine 'n n)
+  (start machine)
+  (get-register-contents machine 'val))
+         
 (--end-- "5.11")
 
