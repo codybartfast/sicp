16163f01e6d778c4757f82553415963d3dc2ce6c
John Donnellan <codybartfast@gmail.com>
2020-01-29 13:51:34 +0000

    Exercise 5.22

--- chapter5/exercise-5.22.scm
+++ chapter5/exercise-5.22.scm
@@ -20,7 +20,72 @@
 
 (-start- "5.22")
 
+(#%require "machine-19.scm")
 
+(println
+ "
+Recursive Append
+================
+")
+
+(let ((machine (make-machine
+                (list
+                  (list 'null? null?)
+                  (list 'car car)
+                  (list 'cdr cdr)
+                  (list 'cons cons))
+                '((assign continue (label done))
+                  append
+                    (test (op null?) (reg x))
+                    (branch (label x-null))
+                    (assign head (op car) (reg x))
+                    (assign x (op cdr) (reg x))
+                    (save continue)
+                    (assign continue (label cons))
+                    (save head)
+                    (goto (label append))
+                  x-null
+                    (goto (reg continue))
+                  cons
+                    (restore head)
+                    (restore continue)
+                    (assign y (op cons) (reg head) (reg y))
+                    (goto (reg continue))
+                  done))))
+
+  (set-register-contents! machine 'x '(a b c d))
+  (set-register-contents! machine 'y '(e f g h))
+  (start machine)
+  (get-register-contents machine 'y))
+
+(println
+ "
+Splice Append
+=============
+")
+
+(let ((machine (make-machine
+                (list
+                  (list 'null? null?)
+                  (list 'set-cdr! set-cdr!)
+                  (list 'cdr cdr))
+                '((save x)
+                  last-pair
+                    (assign x-cdr (op cdr) (reg x))
+                    (test (op null?) (reg x-cdr))
+                    (branch (label splice))
+                    (assign x (reg x-cdr))
+                    (goto (label last-pair))
+                  splice
+                    (perform (op set-cdr!) (reg x) (reg y))
+                    (restore x)))))
+
+  (set-register-contents! machine 'x '(a b c d))
+  (set-register-contents! machine 'y '(e f g h))
+  (start machine)
+  (get-register-contents machine 'x))
+
+(println "")
 
 (--end-- "5.22")
 
