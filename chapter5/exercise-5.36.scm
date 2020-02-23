22bd608220ae726dc617229b8a576c6aeac81ca1
John Donnellan <codybartfast@gmail.com>
2020-02-23 09:00:45 +0000

    3.36 - wip

--- chapter5/exercise-5.36.scm
+++ chapter5/exercise-5.36.scm
@@ -21,7 +21,25 @@
 
 (-start- "5.36")
 
+(println
+ "
+Our compiler evaluates operands from right to left.  By evaluating the first
+arg last we can simply, efficiently cons it with the other args so it is the
+first time in argl.
 
+This is implemented in construct-arglist which is called from
+compile-application
 
+So with our compiler we evaluate in reverse order so that we can efficiently
+construct a list in the correct order.  With the ec-evaluator this
+optimisaton wasn't available as we don't know beforehand how many arguments
+there are.  As a result primitive application and extend environment needed
+to be updated to deal with a 'reversed' argument list.
+
+Evaluating arguments left to right will result in work equivelant to
+'reverse' if the arguments are in their declared order in argl.  Even if the
+arguments were stored in revese order in argl then the work of reversing
+would effectivel have to be done by primitive apply or extend environment.
+")
 (--end-- "5.36")
 
