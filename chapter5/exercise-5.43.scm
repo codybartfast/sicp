876b40410693a317445979cad2b530c122084891
John Donnellan <codybartfast@gmail.com>
2020-02-29 17:50:54 +0000

    5.43 - wip

--- chapter5/exercise-5.43.scm
+++ chapter5/exercise-5.43.scm
@@ -23,7 +23,57 @@
 
 (-start- "5.43")
 
+(#%require "compiler-39.scm")
 
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+(define make-call cons)
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+(define (definition? exp)
+  (tagged-list? exp 'define))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)
+                   (cddr exp))))
+
+(define (scan-out-defines exp)
+  (define (scan exp new-members vars)
+    (if (null? exp)
+        (cons new-members vars)
+        (let ((member (car exp)))
+          (if (definition? member)
+              (scan (cdr exp)
+                    (cons
+                     (list 'set!
+                           (definition-variable member)
+                           (definition-value member))
+                     new-members)
+                    (cons (definition-variable member) vars))
+              (scan (cdr exp)
+                    (cons member new-members)
+                    vars)))))
+  (let ((scan-rslt (scan exp '() '())))
+    (let ((new-body (reverse (car scan-rslt)))
+          (vars (reverse (cdr scan-rslt))))
+      (let ((vals (map (lambda (var) '*unassigned*) vars)))
+        (if (null? vars)
+            exp
+            (make-call (make-lambda vars new-body) vals))))))
+
+(scan-out-defines
+ '(
+    (define a n)
+    (define (double n) (* 2 n))
+    (double a)))
 
 (--end-- "5.43")
 
