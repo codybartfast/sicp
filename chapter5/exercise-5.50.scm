11c237d76d2825b77cf315b3ce637b0e176169f5
John Donnellan <codybartfast@gmail.com>
2020-03-09 14:19:04 +0000

    5.50 - add demo code

--- chapter5/exercise-5.50.scm
+++ chapter5/exercise-5.50.scm
@@ -23,6 +23,60 @@
 (#%require "machine-50.scm")
 (#%require "compiler-50.scm")
 (#%require "ec-evaluator-50.scm")
+(#%require "mc-evaluator-50-exp.scm")
+
+(println "Compiling metacircular-evaluator source ...")
+(define mc-eval-code
+  (statements-with-next mc-evaluator-exp))
+
+(define program
+  (append '((assign env (op get-global-environment)))
+          mc-eval-code))
+
+(println "Making machine ...")
+(define machine (make-machine eceval-operations program))
+
+(println "Starting matacirculator evaluator REPL ...")
+
+
+(start machine)
+
+#|
+
+============================================================================
+Snippets To Use In The REPL
+----------------------------------------------------------------------------
+
+(define (pick-fruit)
+  (define trace '())
+  (define (get-apple)
+    (set! trace (cons "'getting apple'" trace))
+    "apple")
+  (define (get-cherry)
+    (set! trace (cons "'getting cherry'" trace))
+    "cherry")
+  (define (first-or-second first second which)
+    (cond ((equal? which 'first) (first))
+          (else (second))))
+  (list
+   (list
+    (first-or-second get-apple get-cherry 'first)
+    (first-or-second get-apple get-cherry 'not-first))
+   trace))
+
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+
+(pick-fruit)
+
+(factorial 5)
+
+
+============================================================================
+
+|#
 
 (--end-- "5.50")
-
